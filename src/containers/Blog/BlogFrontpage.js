527b17f47d0346fd9ff50efa3d92baeeff77f320
Citrullin <philipp-blum@jakiku.com>
2016-09-03 23:58:45 +0200

    Add Article Thumbnail List to Blog Frontpage Container

--- src/containers/Blog/BlogFrontpage.js
+++ src/containers/Blog/BlogFrontpage.js
@@ -1,12 +1,16 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
 
+import ArticleThumbnailList from '../../components/Article/ArticleThumbnailList';
+
 export default class BlogFrontpage extends Component{
     
     render(){
         return (
             <div class="blogFrontpage">
                 Blog Frontpage
+                <h2>Article</h2>
+                <ArticleThumbnailList/>
             </div>
         );
     }
